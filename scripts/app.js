a733597e2e96ceb6500f62da37904338582335ce
shivam4srtech <shivam.srtech@gmail.com>
2024-12-27 14:38:19 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+//*******************POP_UP COUPONS****************************
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
